3071e431404483c0bd3bff9725e113b0af73e440
Mark Nalyanya <mark.nalyanya@hp.com>
2018-10-23 17:44:22 +0200

    Update to reflect changes in upstream
    Many changes are happening in Puppetlabs images.
    Once we get all the necessary changes, we might not need to do any
    custom docker images.

--- puppetserver/docker/puppetserver/docker-entrypoint.sh
+++ puppetserver/docker/puppetserver/docker-entrypoint.sh
@@ -26,18 +26,48 @@ if test -n "${PUPPETSERVER_HOSTNAME}"; then
   /opt/puppetlabs/bin/puppet config set server "$PUPPETSERVER_HOSTNAME"
 fi
 
-
-#Set Puppet ENVIRONMENT in the stack yaml file: SERVER_ENVIRONMENT
-# Maybe faster to use special environment without modules for builds
-if test -n "${SERVER_ENVIRONMENT}" ; then
-  SERVER_ENVIRONMENT="${SERVER_ENVIRONMENT}"
-else
-  SERVER_ENVIRONMENT="production"  # default environment
-fi
-
 # Set modulepath if provided
 if test -n "${BASEMODULEPATH}" ; then
   puppet config set basemodulepath "${BASEMODULEPATH}" --section main
+fi
+
+# Disable local CA if using external CA or multi-master configuration
+# CA service enabled by default.
+# Inheritance of environment variable by stack deployment remote client from host seems not supported.
+if test "${CA_DISABLE}" = "true"; then
+  sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/services.d/ca.cfg
+
+# MUST provide CA_SERVER hostname
+# Use fqdn hostname to specify CA servers.
+  if test -n "${CA_SERVER}" ; then
+    # Generate SSL certificates if missing.
+    # May need manual signing on the CA server
+    # But first wait for CA server to be ready
+    fqdn=$(facter fqdn)
+    if [ ! -f "/etc/puppetlabs/puppet/ssl/certs/$fqdn.pem" ]; then
+      while ! nc -z "$CA_SERVER" 8140; do
+        sleep 1
+      done
+
+      puppet agent --noop --server=$CA_SERVER
+    fi
+    # Workaround fix on non-ca Puppetmasters. Default ssl-crl-path=/etc/puppetlabs/puppet/ssl/ca/ca_crl.pem
+    if [ ! -d "/etc/puppetlabs/puppet/ssl/ca" ]; then
+      mkdir /etc/puppetlabs/puppet/ssl/ca
+      ln -s /etc/puppetlabs/puppet/ssl/crl.pem /etc/puppetlabs/puppet/ssl/ca/ca_crl.pem
+    fi
+  else
+    echo "Warning: NO CA_SERVER is provided"
+    echo "         Please provide CA_SERVER hostname"
+    echo "         Otherwise any Certificate Signing Request from this container will fail"
+  fi
+else
+# Configure CA server. This is the default behavior
+  # Configure puppet to use a certificate autosign script (if it exists)
+  # AUTOSIGN=true|false|path_to_autosign.conf
+  if test -n "${AUTOSIGN}" ; then
+    puppet config set autosign "$AUTOSIGN" --section master
+  fi
 fi
 
 # Allow setting the dns_alt_names for the server's certificate. This
@@ -56,74 +86,6 @@ if test -n "${DNS_ALT_NAMES}"; then
         fi
     fi
 fi
-
-
-# Configure CA server
-# Inheritance of environment variable by stack deployment remote client from host seems not supported.
-# Use fqdn hostname to specify CA servers.
-# MUST provide at least one CA_SERVER
-# if test -n "${CA_SERVER}" ; then
-if [ -n "${CA_SERVER}" ] || [  -n "${CA_SERVER1}" ] || [ -n "${CA_SERVER2}" ] ; then
-
-  if [  -n "$CA_SERVER" ]; then
-    CA_SERVER=${CA_SERVER}
-
-  elif [ -n "${CA_SERVER1}" ]; then
-
-    CA_SERVER=${CA_SERVER1}
-  else
-    CA_SERVER=${CA_SERVER2}
-  fi
-
-  
-  # Configure CA_SERVER as main Puppet server for all Puppet masters
-  puppet config set server ${CA_SERVER} --section agent
-
-else
-echo "CA_SERVER is mandatory in docker-compose.yml\n"
-echo "Exiting!! \n"
-exit
-fi
-
-HOSTNAME=`hostname -f`
-
-if [ "${HOSTNAME}" = "$CA_SERVER" ] || [ "${HOSTNAME}" = "$CA_SERVER1" ] || [ "${HOSTNAME}" = "$CA_SERVER2" ]; then
-
-# Configure puppet to use a certificate autosign script (if it exists)
-# AUTOSIGN=true|false|path_to_autosign.conf
-  if test -n "${AUTOSIGN}" ; then
-    puppet config set autosign "$AUTOSIGN" --section master
-  fi
-else
-# Configs for non-CA Puppet masters
-puppet config set CA false --section master
-puppet config set ca_server ${CA_SERVER} --section main
-  if test -n "${CA_PORT}" ; then
-    puppet config set ca_port ${CA_PORT} --section main
-  else
-  CA_PORT=8140
-  fi
-
-# Disable local CA
-sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/services.d/ca.cfg
-
-#Generate SSL certificates. Will need manual signing
-# But first wait for CA server to be ready
-while ! nc -z "$CA_SERVER" $CA_PORT; do
-sleep 1
-done
-
-puppet agent -t --noop --environment=$SERVER_ENVIRONMENT --server=$CA_SERVER
-
-fi
-
-
-
-# Workaround fix on non-ca Puppetmasters. Default ssl-crl-path=/etc/puppetlabs/puppet/ssl/ca/ca_crl.pem
-  if [ ! -d "/etc/puppetlabs/puppet/ssl/ca" ]; then
-    mkdir /etc/puppetlabs/puppet/ssl/ca
-    ln -s /etc/puppetlabs/puppet/ssl/crl.pem /etc/puppetlabs/puppet/ssl/ca/ca_crl.pem
-  fi
 
 #Add cron
 if test -n "${CRON_ENTRY}"; then
